944f1313b69acb85679e5e7d97fa122b4ed48eb5
hzzhuqi1 <hzzhuqi1@corp.netease.com>
2017-06-01 21:09:47 +0800

    #misc ttttt

--- config-service-web/src/main/java/com/qee/controller/TestController.java
+++ config-service-web/src/main/java/com/qee/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.qee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +17,17 @@ public class TestController {
     @Value("${from}")
     private String from;
 
+    @Autowired
+    private Environment environment;
+
 
     @RequestMapping("/from")
     public String from(){
         return this.from;
+    }
+
+    @RequestMapping("/from2")
+    public String from2(){
+        return environment.getProperty("from","undefined");
     }
 }
---
+++ sleuth-service-web/src/main/java/com/qee/ZipkinServerApp.java
@@ -0,0 +1,20 @@
+package com.qee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * Created by hzzhuqi1 on 2017/5/22.
+ */
+@EnableTurbine
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinServerApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApp.class, args);
+    }
+}
